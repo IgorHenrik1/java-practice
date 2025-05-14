14ecd56db63b7bad13be5b527e7323070f89bef9
Igor Henrik <igorhenrikg1@gmail.com>
2025-05-14 18:05:44 -0300

    variáveis

---
+++ src/je02_variaveis/VariaveisApp.java
@@ -0,0 +1,12 @@
+package je02_variaveis;
+
+public class VariaveisApp {
+    public static void main(String[] args) {
+        String nome = "Igor";
+        String nomeCompleto = "Igor Gonçalves";
+        // Variaveis que não podem ser alteradas
+        final String BR = "Brasil";
+        final String BR_PAIS = "Brasil";
+
+    }
+}
