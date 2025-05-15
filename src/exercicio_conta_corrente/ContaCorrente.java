995c4a8d7dc5a545f16011dd1213a61f5445c051
Igor Henrik <igorhenrikg1@gmail.com>
2025-05-15 09:10:04 -0300

    Correção do exercício

--- src/exercicio_conta_corrente/ContaCorrente.java
+++ src/exercicio_conta_corrente/ContaCorrente.java
@@ -1,48 +1,36 @@
 package exercicio_conta_corrente;
 
-import java.util.Scanner;
+import java.time.LocalDate;
 
 public class ContaCorrente {
-    String numeroConta = "1234";
-    int agencia = 12;
-    String nomeCliente = "Igor Gonçalves";
-    String dataNascimento = "03/04/2002";
-    double saldoConta = 1000.00;
+    Integer numero;
+    Integer agencia;
+    String nomeCliente;
+    LocalDate dataNascimento;
+    private Double saldo;
+    boolean ativo = true;
 
-    public String Entrar() {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Digite o número da sua conta:");
-        String numeroContaRecebido = scanner.nextLine();
-
-        System.out.println("Digite o número da sua agência:");
-        int agenciaRecebida = scanner.nextInt();
-
-        if (numeroConta.equals(numeroContaRecebido) && agencia == agenciaRecebida) {
-            System.out.println("Bem vindo " + nomeCliente);
-            System.out.println("Saldo disponivel " + saldoConta);
-            return "Logado com sucesso";
-        }
-
-        System.out.println("Falhou");
-        return "Falhou";
+    Double consultarSaldo(){
+        return saldo;
     }
 
-    public void Sacar(){
-        Scanner scanner = new Scanner(System.in);
+    //List consultarExtrato(LocalDate dataInicial, LocalDate dataFinal){}
 
-        System.out.println("Qual valor deseja sacar: ");
-        double saqueSolicitado = scanner.nextInt();
-
-        if(saqueSolicitado > saldoConta){
-            System.out.println("erro: saldo indisponivel");
-            return;
-        }
-
-        saldoConta = saldoConta - saqueSolicitado;
-        System.out.println("Saque realizado com sucesso");
-        System.out.println("Saldo disponivel: " + saldoConta);
+    void cancelar(String justificativa){
+        //algoritimo irá manipular o atributo
+        //ativo para que se torne false
     }
 
-    
+    void transferir(ContaCorrente contaDestino, Double valorTransferido){
+
+    }
+
+    void sacar(Double valorSolicitado){
+
+    }
+
+
+
+
+
 }
