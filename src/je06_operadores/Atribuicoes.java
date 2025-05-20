8c6e8d1c6b8c866f99a9806f6819672a0841eca3
Igor Henrik <igorhenrikg1@gmail.com>
2025-05-20 15:18:14 -0300

    Operadores de atribuição

---
+++ src/je06_operadores/Atribuicoes.java
@@ -0,0 +1,15 @@
+package je06_operadores;
+
+import java.util.Date;
+
+public class Atribuicoes {
+    public static void main(String[] args) {
+
+        String nome = "GLEYSON";
+        int idade = 22;
+        double peso = 68.5;
+        char sexo = 'M';
+        boolean doadorOrgao = false;
+        Date dataNascimento = new Date();
+    }
+}
